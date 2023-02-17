12d80f871e5822d4032635ce1e99ac4c9e86e4fe
jcarlos21 <j.carlos2020@live.com>
2023-02-17 00:59:08 +0000

    AccessListRule

--- netbox_access_lists/models.py
+++ netbox_access_lists/models.py
@@ -12,3 +12,52 @@ class AccessList(NetBoxModel):
 
     def __str__(self):
         return self.name
+
+
+class AccessListRule(NetBoxModel):
+    access_list = models.ForeignKey(
+        to=AccessList,
+        on_delete=models.CASCADE,
+        related_name='rules'
+    )
+    index = models.PositiveIntegerField()
+    protocol = models.CharField(
+        max_length=30,
+        blank=True
+    )
+    source_prefix = models.ForeignKey(
+        to='ipam.Prefix',
+        on_delete=models.PROTECT,
+        related_name='+',
+        blank=True,
+        null=True
+    )
+    source_ports = ArrayField(
+        base_field=models.PositiveIntegerField(),
+        blank=True,
+        null=True
+    )
+    destination_prefix = models.ForeignKey(
+        to='ipam.Prefix',
+        on_delete=models.PROTECT,
+        related_name='+',
+        blank=True,
+        null=True
+    )
+    destination_ports = ArrayField(
+        base_field=models.PositiveIntegerField(),
+        blank=True,
+        null=True
+    )
+    action = models.CharField(
+        max_length=30
+    )
+    description = models.CharField(
+        max_length=500,
+        blank=True
+    )
+    class Meta:
+        ordering = ('access_list', 'index')
+        unique_together = ('access_list', 'index')
+    def __str__(self):
+        return f'{self.access_list}: Rule {self.index}'
